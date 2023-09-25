a056f6de7abbab2143b3541f51a11c35a96073be
hanihassan01 <hani77327@gmail.com>
2023-09-25 13:14:48 +0530

    yhb

---
+++ lib/p1.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  int _counter = 0;
+  void _incrementCounter() {
+    setState(() {
+     
+      _counter++;
+    });
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        shape: const RoundedRectangleBorder(
+            borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(8),
+                bottomRight: Radius.circular(8))),
+        title: Center(
+            child: const Text(
+          "counter ui ",
+          style: TextStyle(fontWeight: FontWeight.w900, fontSize: 20),
+        )),
+        titleTextStyle: TextStyle(color: Color.fromARGB(255, 255, 248, 248)),
+        backgroundColor: Color.fromARGB(255, 73, 133, 82),
+      ),
+      backgroundColor: Color.fromARGB(255, 210, 229, 245),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Column(
+              children: [
+               
+                Container(child:Column(mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text( '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,),
+                  ],
+                ),
+                  width: 90,
+                  height: 70,
+                  color: Color.fromARGB(224, 109, 159, 106),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
